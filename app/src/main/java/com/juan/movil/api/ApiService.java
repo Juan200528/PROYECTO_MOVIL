62b8a4a64105b5cb9900dcb55c2124ec7f5a4ea3
Juan <juand200528@gmail.com>
2025-05-21 07:54:33 -0500

    Arreglo

--- app/src/main/java/com/juan/movil/api/ApiService.java
+++ app/src/main/java/com/juan/movil/api/ApiService.java
@@ -1,12 +1,16 @@
 package com.juan.movil.api;
 
-import com.juan.movil.model.RegistroRequest;
-import com.juan.movil.model.RegistroResponse;
 import com.juan.movil.model.LoginRequest;
 import com.juan.movil.model.LoginResponse;
+import com.juan.movil.model.RegistroRequest;
+import com.juan.movil.model.RegistroResponse;
+import com.juan.movil.model.ListaResponse;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiService {
@@ -18,4 +22,8 @@ public interface ApiService {
     // Endpoint para iniciar sesión
     @POST("/api/auth/login")
     Call<LoginResponse> loginUsuario(@Body LoginRequest loginRequest);
+
+    // Endpoint para obtener actividades (lista de todos los usuarios)
+    @GET("/api/actividad")
+    Call<List<ListaResponse>> obtenerActividades();
 }
