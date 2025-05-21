bbd3ef160bb425bdcb298243ce5e170f4a832e76
Juan <juand200528@gmail.com>
2025-05-21 08:37:11 -0500

    Configuracion Notificacion

---
+++ app/src/main/java/com/juan/movil/ui/configuracion/ConfiguracionViewModel.java
@@ -0,0 +1,47 @@
+package com.juan.movil.ui.configuracion;
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+//import com.example.movil_figma.db.ManagerDb;
+//import com.example.movil_figma.model.Notificacion;
+import java.util.List;
+
+public class ConfiguracionViewModel extends AndroidViewModel {
+    private ManagerDb managerDb;
+    private MutableLiveData<List<Notificacion>> notificaciones;
+
+    public ConfiguracionViewModel(Application application) {
+        super(application);
+        managerDb = new ManagerDb(application);
+        managerDb.open();
+        notificaciones = new MutableLiveData<>();
+    }
+
+    public LiveData<List<Notificacion>> getNotificaciones() {
+        return notificaciones;
+    }
+
+    public void cargarNotificaciones(int userId) {
+        List<Notificacion> notificacionList = managerDb.obtenerNotificacionesPorUsuario(userId);
+        notificaciones.setValue(notificacionList);
+    }
+
+    public void insertarNotificacion(Notificacion notificacion) {
+        managerDb.insertarNotificacion(notificacion);
+        actualizarNotificaciones(notificacion.getIdUsuario());
+    }
+
+    public void actualizarNotificaciones(int userId) {
+        managerDb.actualizarDiasRestantes();
+        List<Notificacion> notificacionList = managerDb.obtenerNotificacionesPorUsuario(userId);
+        notificaciones.setValue(notificacionList);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        managerDb.close();
+    }
+}
